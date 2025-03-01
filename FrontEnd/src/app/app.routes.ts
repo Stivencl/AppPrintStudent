d98531c7775f4a6947f081aea5eb9e1ca46e302e
Stivencl <stevfoun@gmail.com>
2025-03-01 15:25:01 -0500

    RutasAngular

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,25 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule} from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SingInComponent } from './components/singin/sing-in.component';
+import { DashBoardComponent } from './components/dashboard/dash-board.component';
+import { MaintenanceComponent } from './components/maintenance/maintenance.component';
+import { ErrorpageComponent } from './components/errorpage/errorpage.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'singin', component: SingInComponent},
+    {path: 'dashboard', component: DashBoardComponent },
+    {path: 'maintenance', component: MaintenanceComponent},
+    {path: 'errorpage', component: ErrorpageComponent},
+    {path: '**', redirectTo: '/errorpage', pathMatch:'full'},
+
+    
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  
+})
+export class AppRoutingModule{}
---
+++ FrontEnd/src/app/components/errorpage/errorpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-errorpage',
+  imports: [],
+  templateUrl: './errorpage.component.html',
+  styleUrl: './errorpage.component.scss'
+})
+export class ErrorpageComponent {
+
+}
